6fa1f5b15ae1c5f4ad95942ba98fbebf281da5df
Danztee <olowoniyidaniel@gmail.com>
2024-07-15 22:01:46 +0100

    feat: sponsor page complete

--- src/app/sponsors/page.tsx
+++ src/app/sponsors/page.tsx
@@ -9,5 +9,43 @@ import Link from "next/link";
 import { BsArrowUpRight } from "react-icons/bs";
 
 export default function SponsorsPage() {
-  return <div className="px-4 lg:px-20 container mx-auto"></div>;
+  return (
+    <div className="px-4 lg:px-20 container mx-auto">
+      <section className="lg:w-[800px] mx-auto pt-10">
+        <h1 className="text-wrap text-center mb-4 text-3xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-[74px]">
+          Our sponsors
+        </h1>
+
+        <p className="text-wrap text-center text-[18px] text-primary-gray font-bold">
+          We extend our heartfelt gratitude to our esteemed sponsors for their
+          invaluable support. Your partnership helps make DevFest Ilorin 2024 an
+          extraordinary event, fostering innovation and connecting the tech
+          community. Thank you for being a crucial part of our journey!
+        </p>
+
+        <DateAndLocation />
+      </section>
+
+      <section className="py-10 lg:py-20">
+        <Image src={devfestFrame} alt="devfest-frame" />
+
+        <div className="mt-10 flex items-center justify-center">
+          <Image alt="sponsor" src={sponsor1} className="lg:w-[50%]" />
+        </div>
+
+        <div className="mt-6 flex items-center justify-center">
+          <Button
+            as={Link}
+            href=""
+            borderRadius={50}
+            px={10}
+            py={8}
+            className="!bg-primary-body !text-white hover:opacity-80"
+          >
+            Sponsor Us <BsArrowUpRight />
+          </Button>
+        </div>
+      </section>
+    </div>
+  );
 }
