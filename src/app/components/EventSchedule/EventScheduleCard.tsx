95357085daf37b7537222ba620a074f4ecb9627c
Danztee <olowoniyidaniel@gmail.com>
2024-10-16 13:32:21 +0100

    featL schedues

--- src/app/components/EventSchedule/EventScheduleCard.tsx
+++ src/app/components/EventSchedule/EventScheduleCard.tsx
@@ -50,7 +50,19 @@ export const ShortEventScheduleCard: React.FC<ShortEventScheduleCardProps> = ({
   return (
     <aside className="bg-white rounded-xl p-4 h-full">
       <div className="grid lg:grid-cols-2 lg:gap-10">
-        <h1 className="text-2xl font-bold text-[#136FDE]">{session}</h1>
+        <h1
+          className={`text-2xl font-bold ${
+            session === "Engineering Track"
+              ? "text-[#32A852]"
+              : session === "Cybersecurity Track"
+              ? "text-[#EA4435]"
+              : session === "AI/ML"
+              ? "text-[#F8AB19]"
+              : "text-[#136FDE]"
+          }`}
+        >
+          {session}
+        </h1>
 
         <div>
           <p className="text-[#5D5D5D] font-light flex items-center gap-2 text-sm">
@@ -138,6 +150,63 @@ export const PanelEventScheduleCard: React.FC<PanelEventScheduleCardProps> = ({
             ))}
           </div>
         </div>
+      </div>
+    </aside>
+  );
+};
+
+type IntroductionEventScheduleCardProps = {
+  title: string;
+  time: string;
+  className: string;
+};
+
+export const IntroductionEventScheduleCard: React.FC<
+  IntroductionEventScheduleCardProps
+> = ({ title, time, className }) => {
+  return (
+    <aside className={className}>
+      <div className="bg-white rounded-xl p-4 h-full flex flex-col justify-center items-center">
+        <h1 className="text-2xl font-bold text-[#136FDE]">{title}</h1>
+
+        <div className="flex justify-center items-center gap-6 mt-4">
+          <div className="flex items-center gap-1  mt-2 lg:mt-0">
+            <div className="w-[30px] h-[30px] rounded-full bg-[#D9D9D9]" />
+            <p className="font-medium text-sm">Host</p>
+          </div>
+
+          {time.trim() !== "" && (
+            <p className="text-[#5D5D5D] font-light flex items-center gap-2 justify-center">
+              <Image src={timeIcon} alt="time" className="w-[15px]" />{" "}
+              <span>{time}</span>
+            </p>
+          )}
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+type TrackEventScheduleCardProps = {
+  title: string;
+  className: string;
+  bgColor: string;
+};
+
+export const TrackEventScheduleCard: React.FC<TrackEventScheduleCardProps> = ({
+  title,
+  className,
+  bgColor,
+}) => {
+  return (
+    <aside className={className}>
+      <div
+        className="rounded-xl p-8 h-full flex flex-col justify-center items-center"
+        style={{
+          backgroundColor: bgColor,
+        }}
+      >
+        <h1 className="text-3xl text-center font-bold text-white">{title}</h1>
       </div>
     </aside>
   );
--- src/app/components/EventSchedule/Workshop.tsx
+++ src/app/components/EventSchedule/Workshop.tsx
@@ -1,56 +1,103 @@
 import React from "react";
 import Image from "next/image";
 import { Button } from "@chakra-ui/react";
-import { BsArrowUpRight } from "react-icons/bs";
+import { BsArrowUpRight, BsInfoCircle } from "react-icons/bs";
 import lanyard from "@public/icons/lanyard-white-bg.svg";
 import { ctaLinks } from "@/data";
+import {
+  LongEventScheduleCard,
+  IntroductionEventScheduleCard,
+  TrackEventScheduleCard,
+  ShortEventScheduleCard,
+} from "./EventScheduleCard";
+import { engineeringSchedules, techAndAiSchedules } from "@/data/schedule";
 
 const Workshop: React.FC = () => {
   return (
-    <aside
-      className="border-4 border-[#1E1E1E] rounded-[25px] p-4"
-      style={{
-        backgroundColor: "#F8D8D8",
-      }}
-    >
-      <div className="flex items-center justify-between">
-        <h1 className="text-3xl font-bold mt-4 text-black">Day 1</h1>
-        <div className="p-2 rounded-full w-fit border-2 border-black text-primary-body capitalize  bg-[#57CAFF]">
-          Workshop
-        </div>
+    <div>
+      <div className="grid lg:grid-cols-2 gap-4 pb-10 lg:pb-20">
+        <LongEventScheduleCard
+          title="Arrival & Registration"
+          time="8:00 AM - 9:00 AM"
+          className="lg:col-span-2"
+        />
+        <IntroductionEventScheduleCard
+          time="9:00 AM - 9:30 AM"
+          title="Introduction"
+          className="lg:col-span-2"
+        />
+        <p className="text-[#EA4435] text-sm text-center col-span-2 flex items-center gap-2 justify-center italic">
+          <BsInfoCircle color="#EA4435" />
+
+          <span>
+            All tracks would be going on simultaneously in different breakout
+            rooms.
+          </span>
+        </p>
+        <TrackEventScheduleCard
+          title="Engineering Track & AI"
+          bgColor="#32A852"
+          className="lg:col-span-2"
+        />
+        {engineeringSchedules.map((schedule, index) => (
+          <ShortEventScheduleCard key={index} {...schedule} />
+        ))}
+        <TrackEventScheduleCard
+          title="Tech & AI"
+          bgColor="#136FDE"
+          className="lg:col-span-2"
+        />
+        {techAndAiSchedules.map((schedule, index) => (
+          <ShortEventScheduleCard key={index} {...schedule} />
+        ))}
       </div>
 
-      <div className="flex flex-col justify-center items-center space-y-10">
-        <Image src={lanyard} alt="lanyard" className="w-[300px] mt-14" />
+      <div
+        className="border-4 border-[#1E1E1E] rounded-[25px] p-4"
+        style={{
+          backgroundColor: "#F8D8D8",
+        }}
+      >
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold mt-4 text-black">Day 1</h1>
+          <div className="p-2 rounded-full w-fit border-2 border-black text-primary-body capitalize  bg-[#57CAFF]">
+            Workshop
+          </div>
+        </div>
 
-        <h2
-          className="font-bold text-5xl my-8"
-          style={{
-            color: "#FF7DAF",
-            textShadow: "1px 0 black, -1px 0 black, 0 1px black, 0 -1px black",
-          }}
-        >
-          Free
-        </h2>
+        <div className="flex flex-col justify-center items-center space-y-10">
+          <Image src={lanyard} alt="lanyard" className="w-[300px] mt-14" />
 
-        <div className="flex flex-col justify-center items-center">
-          <p className="text-lg text-primary-body">
-            Full day of workshop in web, mobile, cloud, Design
-          </p>
-          <Button
-            as="a"
-            href={ctaLinks.workshop.link}
-            borderRadius={50}
-            px={8}
-            py={8}
-            className="!text-white hover:opacity-80 mt-4 flex items-center gap-2 w-fit"
+          <h2
+            className="font-bold text-5xl my-8"
+            style={{
+              color: "#FF7DAF",
+              textShadow:
+                "1px 0 black, -1px 0 black, 0 1px black, 0 -1px black",
+            }}
           >
-            Register Now
-            <BsArrowUpRight />
-          </Button>
+            Free
+          </h2>
+
+          <div className="flex flex-col justify-center items-center">
+            <p className="text-lg text-primary-body">
+              Full day of workshop in web, mobile, cloud, Design
+            </p>
+            <Button
+              as="a"
+              href={ctaLinks.workshop.link}
+              borderRadius={50}
+              px={8}
+              py={8}
+              className="!text-white hover:opacity-80 mt-4 flex items-center gap-2 w-fit"
+            >
+              Register Now
+              <BsArrowUpRight />
+            </Button>
+          </div>
         </div>
       </div>
-    </aside>
+    </div>
   );
 };
 
